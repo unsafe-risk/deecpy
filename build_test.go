8a1efc6a342c6f0747f3e52b7edfc0c310d4ad43
lemon-mint <ice.lemons.mint@gmail.com>
2022-07-29 01:14:05 +0900

    Use blackhole

--- build_test.go
+++ build_test.go
@@ -1,12 +1,12 @@
 package deecpy
 
 import (
-	"os"
+	"io"
 	"reflect"
 	"testing"
 )
 
 func Test_build(t *testing.T) {
 	a_type := reflect.TypeOf(a)
-	debugBuild(a_type, os.Stderr)
+	debugBuild(a_type, io.Discard)
 }
